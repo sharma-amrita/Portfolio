6bef8395137f5618b9e3f430c96822d6e13e6c99
AmritaSharma10 <sharma2074amrita@gmail.com>
2024-11-30 12:36:57 +0530

    updated

--- src/Component/certificate.jsx
+++ src/Component/certificate.jsx
@@ -3,9 +3,12 @@ import pdf from './pdf/Academic_Award.pdf'
 
 const certificate = () => {
   return (
+    <>
     <div>
+         <h1>Certificated/Awards</h1>
        <a href={"pdf/Academic_Award"} download='Acedamic.pdf' className="btn btn-outline-warning">Download Certificate</a>
     </div>
+    </>
   )
 }
 
