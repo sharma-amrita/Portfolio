982acca4233635c4e5aff7771c042ee8e418c356
AdityaYadav-1 <adi14yadav@gmail.com>
2024-11-30 23:50:21 +0530

    Changes for Project and Certificate Section

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
-import projects from './Doc/projects.json';
-
+import React from "react";
+import projects from "./Doc/projects.json";
 
 const Projects = () => {
   return (
-   <>
-         <div className='container projects my-3' id='projects'>
-              <h4 >PROJECT</h4>
-              <div className="row">
-                {projects.map((Doc)=>(
-                    <>
-                    <div key={projects.id} className="my-4 col-md-4 ">
-                    <div className="card bg-dark text-light" style={{width:'18rem'}}>
-                                <img src={Doc.imageSrc} className="card-img-top" alt=".."/>
-                                <div className="card-body text-center">
-                                <h5 className="card-title">{Doc.title}</h5>
-                                <p className="card-text">{Doc.description}</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                                </div>
-                     </div>
-                    </div>
-                    </>
-                ))}
-
+    <>
+      <div className="container projects my-3" id="projects">
+        <h4>PROJECT</h4>
+        <div className="row d-flex justify-content-center align-content-center">
+          {projects.map((Doc) => (
+            <>
+              <div
+                key={projects.id}
+                className="my-3 col-sm-6 col-md-4 col-lg-3 mx-3"
+              >
+                <div
+                  className="card bg-dark text-light"
+                  style={{ width: "18rem", border:'2px solid yellow', boxShadow:'5px 5px 10px 6px rgba(101,175,10,0.5)'}}
+                >
+                  <div className="img d-flex justify-content-center align-content-center p-3">
+                  <img src={Doc.imageSrc} className="card-img-top" alt=".." 
+                  style={
+                    {
+                      width:'250px',
+                      height:'200px',
+                      border:'2px solid yellow',
+                      borderRadius:'10px'
+                    }
+                  }/>
+                  </div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{Doc.title}</h5>
+                    <p className="card-text">{Doc.description}</p>
+                    <a href={Doc.source} className="btn btn-primary" target="_blank">
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
         </div>
-   </div>
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
--- src/Component/certificate.jsx
+++ src/Component/certificate.jsx
@@ -1,26 +1,56 @@
 import React from "react";
 import Certificates from "./Doc/Certificate.json";
 
-
 const Certificate = () => {
   return (
-    <div className="container certificate" id='certificate'>
-      <h3>Certificates/Awards</h3>
-      {Certificates.map((certi, index) => (
-        <div className="item" key={certi.id}>
-          <div className="item-details">
-            <img src={certi.imageSrc} alt={`Certificate ${index + 1}`} />
-            <h3>{`Certificate ${index + 1}`}</h3>
-            <button
-              className="view-button"
-              onClick={() => window.open(certi.documentLink, "_blank")}
+    <>
+      <div className="container certificate my-3" id="certificate">
+        <h4>Certificates/Awards</h4>
+        <div className="row d-flex justify-content-center align-content-center">
+          {Certificates.map((certi) => (
+            <div
+              key={certi.id}
+              className="my-3 col-sm-6 col-md-4 col-lg-3 mx-3"
             >
-              View
-            </button>
-          </div>
+              <div
+                className="card bg-dark text-light"
+                style={{
+                  width: "18rem",
+                  border: "2px solid yellow",
+                  boxShadow: "5px 5px 10px 6px rgba(101,175,10,0.5)",
+                }}
+              >
+                <div className="img d-flex justify-content-center align-content-center p-3">
+                  <img
+                    src={certi.imageSrc}
+                    className="card-img-top"
+                    alt={`Certificate ${certi.id}`}
+                    style={{
+                      width: "250px",
+                      height: "200px",
+                      border: "2px solid yellow",
+                      borderRadius: "10px",
+                    }}
+                  />
+                </div>
+                <div className="card-body text-center">
+                  {/* <h5 className="card-title">{`Certificate ${certi.id}`}</h5> */}
+                  <p className="card-text">{certi.title}</p>
+                  <a
+                    href={certi.documentLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-primary"
+                  >
+                    View
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
