757e92226e2fb70434bdcad38f285834b26e21b2
AmritaSharma10 <sharma2074amrita@gmail.com>
2024-11-30 13:43:34 +0530

    updated

--- src/Component/certificate.jsx
+++ src/Component/certificate.jsx
@@ -1,11 +1,13 @@
 import  React from 'react';
+import  React from './pdf/academic';
+
  
  const Certificate = () => {
    return (
     <>
     <div>
        <h3>Certificate/Awards</h3>
-    <a href={"pdf/Academic_Award"} download='Academic.pdf' className="btn btn-outline-warning">Download Academic</a>
+    <a href={"pdf/academic"} download='Academic.pdf' className="btn btn-outline-warning">Download Academic</a>
        </div>
     </>
      
